9389c2d08e4f7aa180d66fcf28ebd821da03663f
Anthony Griffith <a.griffith.gonzalez@gmail.com>
2022-01-03 02:06:48 -0600

    changed from react final form to react hooks form and added picture field to the form

--- src/components/forms/ItemCreate.js
+++ src/components/forms/ItemCreate.js
@@ -2,9 +2,20 @@ import { connect } from 'react-redux';
 import '../../styles/ItemForms.css';
 import { addItem } from '../../actions/index';
 import ItemForm from './itemForm';
+import { IMGBB_KEY } from '../../config';
+import axios from 'axios';
 
 const ItemCreate = ({ addItem }) => {
-  const onSubmit = (formValues) => {
+  const onSubmit = async (formValues) => {
+    const reqBody = new FormData();
+    reqBody.set('key', IMGBB_KEY);
+    reqBody.append('image', formValues.image[0]);
+    const { data } = await axios.post(
+      'https://api.imgbb.com/1/upload',
+      reqBody
+    );
+    formValues.img_url = data.data.url;
+    console.log(formValues);
     addItem(formValues);
   };
   return <ItemForm onSubmit={onSubmit} />;
--- src/components/forms/itemForm.js
+++ src/components/forms/itemForm.js
@@ -1,111 +1,66 @@
-import { Form, Field } from 'react-final-form';
-import { connect } from 'react-redux';
 import '../../styles/ItemForms.css';
-const ItemForm = ({ onSubmit, initialValues }) => {
-  const validate = (values) => {
-    const errors = {};
-    if (!values.name) {
-      errors.name = 'Required';
-    }
-    if (!values.description) {
-      errors.description = 'Required';
-    }
-    if (!values.price) {
-      errors.price = 'Required';
-    }
-    return errors;
-  };
+import { useForm } from 'react-hook-form';
+import { useState, useEffect } from 'react';
+
+const ItemForm = ({ onSubmit }) => {
+  const { register, handleSubmit } = useForm();
+
   return (
-    <Form
-      onSubmit={onSubmit}
-      initialValues={initialValues}
-      validate={validate}
-      render={({ handleSubmit }) => (
-        <div className="form-container">
-          <form onSubmit={handleSubmit}>
-            <table>
-              <thead>
-                <tr>
-                  <td align="center" colSpan={2}>
-                    <h3>New Item Form</h3>
-                  </td>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>
-                    <label>Name</label>
-                  </td>
-                  <td>
-                    <Field name="name">
-                      {({ input, meta }) => (
-                        <div>
-                          <input
-                            {...input}
-                            autoComplete="off"
-                            style={
-                              meta.touched && meta.error
-                                ? { border: '1px dotted red' }
-                                : null
-                            }
-                          />
-                        </div>
-                      )}
-                    </Field>
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    <label>Descripcion</label>
-                  </td>
-                  <td>
-                    <Field name="description">
-                      {({ input, meta }) => (
-                        <input
-                          {...input}
-                          autoComplete="off"
-                          style={
-                            meta.touched && meta.error
-                              ? { border: '1px dotted red' }
-                              : null
-                          }
-                        />
-                      )}
-                    </Field>
-                  </td>
-                </tr>
-                <tr>
-                  <td>
-                    <label>Price</label>
-                  </td>
-                  <td>
-                    <Field name="price">
-                      {({ input, meta }) => (
-                        <input
-                          {...input}
-                          type="number"
-                          autoComplete="off"
-                          style={
-                            meta.touched && meta.error
-                              ? { border: '1px dotted red' }
-                              : null
-                          }
-                        />
-                      )}
-                    </Field>
-                  </td>
-                </tr>
-                <tr>
-                  <td colSpan={2} align="center">
-                    <button type="submit">Create</button>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </form>
-        </div>
-      )}
-    />
+    <div className="form-container">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <table>
+          <thead>
+            <tr>
+              <td align="center" colSpan={2}>
+                <h3>New Item Form</h3>
+              </td>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                <label>Name</label>
+              </td>
+              <td>
+                <input {...register('name', { required: true })} />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <label>Descripcion</label>
+              </td>
+              <td>
+                <input {...register('description', { required: true })} />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <label>Price</label>
+              </td>
+              <td>
+                <input
+                  type="number"
+                  {...register('price', { required: true })}
+                />
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <label>Image</label>
+              </td>
+              <td>
+                <input {...register('image', { required: true })} type="file" />
+              </td>
+            </tr>
+            <tr>
+              <td colSpan={2} align="center">
+                <button type="submit">Create</button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </form>
+    </div>
   );
 };
 
