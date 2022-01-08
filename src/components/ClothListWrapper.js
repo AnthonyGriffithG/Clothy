b77111df6abbc969a3a60478f6d9120e51b34de6
Anthony Griffith <a.griffith.gonzalez@gmail.com>
2022-01-07 19:34:38 -0600

    refactored clothlistoptions to clothlistwrapper

--- src/components/ClothListWrapper.js
+++ src/components/ClothListWrapper.js
@@ -3,7 +3,8 @@ import { Link } from 'react-router-dom';
 import { MdAddCircle } from 'react-icons/md';
 import '../styles/ClothList.css';
 
-export default function ClothListOptions() {
+export default function ClothListWrapper({ children }) {
+  const [page, setPage] = useState(0);
   const [fixed, setFixed] = useState(false);
   const changeAddBtn = () => {
     if (window.innerWidth < 600 && window.scrollY >= 70) {
@@ -20,14 +21,22 @@ export default function ClothListOptions() {
     };
   }, []);
   return (
-    <div className="items-options">
-      <div></div>
-      <Link
-        className={`add-item-link ${fixed ? 'fixed' : ''}`}
-        to={'/items/new'}
-      >
-        <MdAddCircle size={fixed ? '3em' : '2em'} />
-      </Link>
+    <div>
+      <div className="items-options">
+        <div></div>
+        <Link
+          className={`add-item-link ${fixed ? 'fixed' : ''}`}
+          to={'/items/new'}
+        >
+          <MdAddCircle size={fixed ? '3em' : '2em'} />
+        </Link>
+      </div>
+      {children}
+      <div className="pagination">
+        <button>{page - 1}</button>
+        {page}
+        <button>{page + 1}</button>
+      </div>
     </div>
   );
 }
